5e6eefc9ba5b66148febf773bbe92e168a0e4447
etoledo <etoledo@bancolombia.com.co>
2020-04-09 19:05:43 -0500

    navbar mobile

--- src/app/header-nav-bar/header-nav-bar.component.ts
+++ src/app/header-nav-bar/header-nav-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-nav-bar.component.scss']
 })
 export class HeaderNavBarComponent implements OnInit {
+  showSidenav = false;
 
   constructor() { }
 
