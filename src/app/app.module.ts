2739ab5a8f8cbd45ef2dbe62fcfb212704eb425d
etoledo <etoledo@bancolombia.com.co>
2020-05-04 19:08:34 -0500

    Solucion sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,32 @@ import { environment } from '../environments/environment';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeaderNavBarComponent } from './header-nav-bar/header-nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     HeaderNavBarComponent,
-    FooterComponent
+    FooterComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header-nav-bar/header-nav-bar.component.ts
+++ src/app/header-nav-bar/header-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header-nav-bar',
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-nav-bar.component.scss']
 })
 export class HeaderNavBarComponent implements OnInit {
-  showSidenav = false;
+  @Output() public sidenavToggle = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onToggleSidenav = () => {
+    this.sidenavToggle.emit();
   }
 
 }
