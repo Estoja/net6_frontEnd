b465bba9646383b4a8f984ba395819c2857a9b4e
etoledo <etoledo@bancolombia.com.co>
2020-05-06 18:53:17 -0500

    Inicio Home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { WhatWeDoComponent } from './whatWeDo/what-we-do/what-we-do.component';
+import { SolutionsComponent } from './solutions/solutions/solutions.component';
+import { MatIconModule } from '@angular/material/icon';
+import { ScrollDownComponent } from './scrollDownIndicator/scroll-down/scroll-down.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, WhatWeDoComponent, SolutionsComponent, ScrollDownComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatIconModule
   ]
 })
 export class HomeModule { }
